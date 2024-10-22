a9d766ce3c37c9600ec489cb55afc88f68552ffd
LydanielTT <lydaniel94@gmail.com>
2024-10-22 12:53:35 +0200

    move logic to drug

--- src/Drug/drug.ts
+++ src/Drug/drug.ts
@@ -7,6 +7,55 @@ class Drug {
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
+  private isBenefit50 = (drugArg) => {
+    return drugArg.benefit < 50;
+  };
+
+  updateBenefitValue = () => {
+    if (this.name != "Herbal Tea" && this.name != "Fervex") {
+      if (this.benefit > 0) {
+        if (this.name != "Magic Pill") {
+          this.benefit = this.benefit - 1;
+        }
+      }
+    } else {
+      if (this.isBenefit50(this)) {
+        this.benefit = this.benefit + 1;
+        if (this.name == "Fervex") {
+          if (this.expiresIn < 11) {
+            if (this.isBenefit50(this)) {
+              this.benefit = this.benefit + 1;
+            }
+          }
+          if (this.expiresIn < 6) {
+            if (this.isBenefit50(this)) {
+              this.benefit = this.benefit + 1;
+            }
+          }
+        }
+      }
+    }
+    if (this.name != "Magic Pill") {
+      this.expiresIn = this.expiresIn - 1;
+    }
+    if (this.expiresIn < 0) {
+      if (this.name != "Herbal Tea") {
+        if (this.name != "Fervex") {
+          if (this.benefit > 0) {
+            if (this.name != "Magic Pill") {
+              this.benefit = this.benefit - 1;
+            }
+          }
+        } else {
+          this.benefit = this.benefit - this.benefit;
+        }
+      } else {
+        if (this.benefit < 50) {
+          this.benefit = this.benefit + 1;
+        }
+      }
+    }
+  };
 }
 
 export default Drug;

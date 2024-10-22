519b99d39d4caa214e171b44f77a8499c7bd80b6
LydanielTT <lydaniel94@gmail.com>
2024-10-22 14:58:13 +0200

    change logic drug, missing test

--- src/Drug/drug.ts
+++ src/Drug/drug.ts
@@ -7,58 +7,52 @@ export class Drug {
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
-  private isBenefit50 = (drugArg) => {
-    return drugArg.benefit < 50;
+  private isBenefitUnder50 = () => {
+    return this.benefit < 50;
+  };
+  private isBenefitOver0 = () => {
+    return this.benefit > 0;
   };
 
   private ratioChangeBenefit = () => {
+    let ratio = 1;
+    if (this.name != "Herbal Tea" && this.name != "Fervex") {
+      ratio = -1;
+    }
+    if (this.expiresIn < 0) {
+      ratio *= 2;
+    }
+    if (this.name == "Fervex") {
+      if (this.expiresIn < 11) {
+        ratio = 2;
+      }
+      if (this.expiresIn < 6) {
+        ratio = 3;
+      }
+      if (this.expiresIn < 0) {
+        ratio = -this.benefit;
+      }
+    }
+    if (this.name == "Herbal Tea") {
+      if (this.expiresIn < 0) {
+        ratio = 2;
+      }
+      this.benefit += ratio;
+      this.benefit = !this.isBenefitUnder50 ? 50 : this.benefit;
+      this.benefit = !this.isBenefitOver0 ? 0 : this.benefit;
+    }
     // -2x if expiresIn <0
     // benefit <0 = 0
     // herbal tea and expiresIn < 0 = +2x
     // >50 = 50
-    // herbal tea && fervex : +2x expiresIn <11 && >6
+    // fervex : +2x expiresIn <11 && >6 +3x
+    // TODO test
   };
   updateBenefitValue = () => {
     if (this.name == "Magic Pill") {
       return;
     }
-    if (this.name != "Herbal Tea" && this.name != "Fervex") {
-      if (this.benefit > 0) {
-        this.benefit = this.benefit - 1;
-      }
-    } else {
-      if (this.isBenefit50(this)) {
-        this.benefit = this.benefit + 1;
-        if (this.name == "Fervex") {
-          if (this.expiresIn < 11) {
-            if (this.isBenefit50(this)) {
-              this.benefit = this.benefit + 1;
-            }
-          }
-          if (this.expiresIn < 6) {
-            if (this.isBenefit50(this)) {
-              this.benefit = this.benefit + 1;
-            }
-          }
-        }
-      }
-    }
-
-    if (this.expiresIn < 0) {
-      if (this.name != "Herbal Tea") {
-        if (this.name != "Fervex") {
-          if (this.benefit > 0) {
-            this.benefit = this.benefit - 1;
-          }
-        } else {
-          this.benefit = this.benefit - this.benefit;
-        }
-      } else {
-        if (this.benefit < 50) {
-          this.benefit = this.benefit + 1;
-        }
-      }
-    }
+    this.ratioChangeBenefit();
     this.expiresIn = this.expiresIn - 1;
   };
 }

086658f83ebfc65d11689b5974a040c39b893d9c
LydanielTT <lydaniel94@gmail.com>
2024-10-22 12:25:48 +0200

    update pharmacy test

--- src/Pharmacy/pharmacy.test.ts
+++ src/Pharmacy/pharmacy.test.ts
@@ -1,10 +1,15 @@
-import Drug from "../Drug/drug";
 import Pharmacy from "./pharmacy";
+
+const Drug = jest.fn().mockReturnValue((name, expiresIn, benefit) => ({
+  name,
+  expiresIn,
+  benefit,
+}));
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)],
+      [new Drug("test", 1, 2)]
     );
   });
 });
